579e527988af1ba6a268e8c9dea4431c90914434
Rudresh <rudresh.narwal@gmail.com>
2024-03-04 20:00:30 +0530

    feat: configured celery, flower and redis

--- foloDjango/settings/local.py
+++ foloDjango/settings/local.py
@@ -7,4 +7,13 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="sVsP1LinBegCrAxKpK45gG_u3ugDaZ9yy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "rudresh@ubuntuonline.co.ke"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "FoloMoney-Core"

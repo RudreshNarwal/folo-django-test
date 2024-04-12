903934dfe6d0ba270d2fe8059f49322dfdc70c66
Rudresh <rudresh.narwal@gmail.com>
2024-04-12 15:49:41 +0530

    code review

--- foloDjango/wsgi.py
+++ foloDjango/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # TODO change this in prod
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foloDjango.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foloDjango.settings.production")
 
 application = get_wsgi_application()

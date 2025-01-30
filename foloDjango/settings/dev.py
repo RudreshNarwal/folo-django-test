00ec9836b454bfb65b3d59d17e43f5a8e5e8ce2d
Rudresh <rudresh.narwal@gmail.com>
2025-01-31 01:44:17 +0530

    Review: Refactored code

--- foloDjango/settings/dev.py
+++ foloDjango/settings/dev.py
@@ -14,8 +14,7 @@ env_file = ROOT_DIR / '.envs/.dev/.django'
 
 # Check if the .env file exists and then read it
 if env_file.is_file():
-    environ.Env.read_env(str(env_file))
-    
+	environ.Env.read_env(str(env_file))
 
 DEBUG = env.bool('DJANGO_DEBUG', False)
 
@@ -135,7 +134,6 @@ USE_TZ = True
 
 SITE_ID = 1  # default site for the project is 1
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -191,75 +189,73 @@ SIMPLE_JWT = {
 }
 
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
-    "formatters": {
-        "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
-        }
-    },
-    "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler",
-        },
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-    },
-    "root": {"level": "INFO", "handlers": ["console"]},
-    "loggers": {
-        "django.request": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-        "django.security.DisallowedHost": {
-            "handlers": ["console", "mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
-        },
-    },
+	"version": 1,
+	"disable_existing_loggers": False,
+	"filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+	"formatters": {
+		"verbose": {
+			"format": "%(levelname)s %(asctime)s %(module)s "
+			          "%(process)d %(thread)d %(message)s"
+		}
+	},
+	"handlers": {
+		"mail_admins": {
+			"level": "ERROR",
+			"filters": ["require_debug_false"],
+			"class": "django.utils.log.AdminEmailHandler",
+		},
+		"console": {
+			"level": "DEBUG",
+			"class": "logging.StreamHandler",
+			"formatter": "verbose",
+		},
+	},
+	"root": {"level": "INFO", "handlers": ["console"]},
+	"loggers": {
+		"django.request": {
+			"handlers": ["mail_admins"],
+			"level": "ERROR",
+			"propagate": True,
+		},
+		"django.security.DisallowedHost": {
+			"handlers": ["console", "mail_admins"],
+			"level": "ERROR",
+			"propagate": True,
+		},
+	},
 }
-
 
 ADMINS = [("Rudresh Narwal", "rudresh@ubuntuonline.co.ke")]
 
 # TODO add domain names of the production server
-# CSRF_TRUSTED_ORIGINS = ["https://folo.money"]
-# for local
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+CSRF_TRUSTED_ORIGINS = ["https://folo.money"]  # for local CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
 SECRET_KEY = env("DJANGO_SECRET_KEY")
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["folo.money"]) + ["localhost", "0.0.0.0", "127.0.0.1"] # for local ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
-CORS_ALLOW_ALL_ORIGINS = True # set false for prod TODO
-CORS_ALLOW_CREDENTIALS = True # set false for prod TODO
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["folo.money"]) + ["localhost", "0.0.0.0",
+                                                                            "127.0.0.1"]  # for local ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+CORS_ALLOW_ALL_ORIGINS = True  # set false for prod TODO
+CORS_ALLOW_CREDENTIALS = True  # set false for prod TODO
 ADMIN_URL = env("DJANGO_ADMIN_URL")
 DATABASES = {"default": env.db("DATABASE_URL")}
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https") # comment this to run in local
-# SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True) # comment this to run in local
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")  # comment this to run in local
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)  # comment this to run in local
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 # TODO: change to 518400 later
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
-    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
+	"DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
 )
 
 SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
-    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
+	"DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
 )
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL",
-    default="FoloMoney Dev <rudresh@ubuntuonline.co.ke>",
+	"DJANGO_DEFAULT_FROM_EMAIL",
+	default="FoloMoney Dev <rudresh@ubuntuonline.co.ke>",
 )
 DEFAULT_EMAIL_RECEIVERS = ["rudresh@ubuntuonline.co.ke", "kevin@ubuntuonline.co.ke", "rudresh.narwal20@gmail.com"]
 
@@ -268,8 +264,8 @@ SITE_NAME = "FoloMoney"
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 
 EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[FoloMoney]",
+	"DJANGO_EMAIL_SUBJECT_PREFIX",
+	default="[FoloMoney]",
 )
 EMAIL_BACKEND = 'django_ses.SESBackend'
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
@@ -279,20 +275,20 @@ AWS_SES_REGION_ENDPOINT = 'email.af-south-1.amazonaws.com'
 EMAIL_USE_TLS = True
 DOMAIN = env("DOMAIN")
 
-BASE_URL=env("BASE_URL")
+BASE_URL = env("BASE_URL")
 
-TRANSUNION_ENDPOINT=env("TRANSUNION_ENDPOINT")
-TRANSUNION_USERNAME=env("TRANSUNION_USERNAME")
-TRANSUNION_PASSWORD=env("TRANSUNION_PASSWORD")
-TRANSUNION_CODE=env("TRANSUNION_CODE")
-TRANSUNION_INFINITY_CODE=env("TRANSUNION_INFINITY_CODE")
+TRANSUNION_ENDPOINT = env("TRANSUNION_ENDPOINT")
+TRANSUNION_USERNAME = env("TRANSUNION_USERNAME")
+TRANSUNION_PASSWORD = env("TRANSUNION_PASSWORD")
+TRANSUNION_CODE = env("TRANSUNION_CODE")
+TRANSUNION_INFINITY_CODE = env("TRANSUNION_INFINITY_CODE")
 
-MPESA_ENDPOINT=env("MPESA_ENDPOINT")
-MPESA_PASSKEY=env("MPESA_PASSKEY")
-MPESA_CLIENT_TOKEN=env("MPESA_CLIENT_TOKEN")
-MPESA_BUSINESS_CODE=env("MPESA_BUSINESS_CODE")
+MPESA_ENDPOINT = env("MPESA_ENDPOINT")
+MPESA_PASSKEY = env("MPESA_PASSKEY")
+MPESA_CLIENT_TOKEN = env("MPESA_CLIENT_TOKEN")
+MPESA_BUSINESS_CODE = env("MPESA_BUSINESS_CODE")
 
-AWS_STORAGE_BUCKET_NAME=env("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME=env("AWS_S3_REGION_NAME")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
 
 OPENAI_API_KEY = env("OPENAI_API_KEY")
